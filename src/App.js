600b534e4b5147e213db71c49670c9e8f1cae419
antonin michel <a.copiercoller@gmail.com>
2022-09-29 02:19:43 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-dark-800 h-25">
+      <h1 className='text-white'>Hello world !</h1>
+    </div>
+  );
+}
+
+export default App;
