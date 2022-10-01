8fb485a1ba94bd79cbf922a408ea7d077e5de554
antonin michel <a.copiercoller@gmail.com>
2022-10-01 07:52:27 +0200

    affinage de l'interface

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,114 @@
-import React from 'react';
-import '../input.css';
+import React from "react";
+import Item from "./Item";
 
 export default function NavBar() {
-  const menu1 = ['menu1'];
-  const [column1, setColumn1] = React.useState([menu1]);
-  // const handleShowColumn1 = () =>{
-  //   setColumn1(menu1.puch('liste1', 'liste2', 'liste3'))
-  // }
+  const [show, setShow] = React.useState(false);
+  const [show2, setShow2] = React.useState(false);
+  const [show3, setShow3] = React.useState(false);
+  const [classe, setClasse] = React.useState("inactive");
+  const handleShow = () => {
+    if (classe === "inactive") {
+      // setShow(true);
+      setClasse("active");
+    } else {
+      // setShow(false);
+      setClasse("inactive");
+    }
+  };
   return (
-    <div className='bg-gray-800 h-10 text-white'><p>{column1}</p></div>
+    <div className="bg-gray-800 h-10 text-white flex">
+      <ul className="z-50 mt-2">
+        <li        >
+          <button onClick={()=>{
+            if(!show){
+              setShow(true)
+            }else{
+              setShow(false)
+            }
+          }}>Association</button>
+        </li>
+        <li
+          className={
+            !show ? "invisible" : "visible"
+          }
+        >
+          Prochains rendez-vous
+        </li>
+        <li
+          className={
+            !show ? "invisible" : "visible"
+          }
+        >
+          Nos activités
+        </li>
+        <li
+          className={
+            !show ? "invisible" : "visible"
+          }
+        >
+          Adhérez
+        </li>
+        <li
+          className={
+            !show ? "invisible" : "visible"
+          }
+        >
+          Nous contacter
+        </li>
+      </ul>
+
+      <ul className="z-50 mt-2">
+        <li        >
+          <button onClick={()=>{
+            if(!show2){
+              setShow2(true)
+            }else{
+              setShow2(false)
+            }
+          }}>Agenda</button>
+        </li>
+        <li
+          className={
+            !show2 ? "invisible" : "visible"
+          }
+        >
+          Ateliers Jardin
+        </li>
+        <li
+          className={
+            !show2 ? "invisible" : "visible"
+          }
+        >
+          Conférence
+        </li>
+        
+      </ul>
+
+      <ul className="z-50 mt-2">
+        <li        >
+          <button onClick={()=>{
+            if(!show3){
+              setShow3(true)
+            }else{
+              setShow3(false)
+            }
+          }}>Les Jardins D'Yvonne</button>
+        </li>
+        <li
+          className={
+            !show3 ? "invisible" : "visible"
+          }
+        >
+          Présentation
+        </li>
+        <li
+          className={
+            !show3 ? "invisible" : "visible"
+          }
+        >
+          La carte
+        </li>
+        </ul>
+    </div>
   );
 }
--- src/views/Home.js
+++ src/views/Home.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="sm:w-100">
       <NavBar />
-      <img src={Logo} alt="logo" className="w-100 h-100" />
+      <img src={Logo} alt="logo" className="sm:w-100 sm:h-100 md:w-80 md:w-80" />
       <h1 className="text-3xl">Bienvenue sur les Jardins D'Yvonne</h1>
     </div>
   );
